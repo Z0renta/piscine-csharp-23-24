5cc65d712e5cb4514d9b4ab088f378f7359b1440
Alexis Cazin <alexis.cazin@ynov.com>
2023-11-21 10:33:37 +0100

    hello world

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class HelloWorld_Exercice
+    {
+        public static string HelloWorld()
+        {
+            // Your code
+            Console.WriteLine("Hello World !");
+            return "Hello World !";
+        }
+    }
+}
---
+++ test.cs
@@ -0,0 +1,13 @@
+using System;
+using CSharpDiscovery.Quest01;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HelloWorld_Exercice.HelloWorld());
+        }
+    }
+}
